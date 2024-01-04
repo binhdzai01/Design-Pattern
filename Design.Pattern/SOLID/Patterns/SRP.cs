8b961260565dacc2a775f6cc4d3ae7f5555fb45d
embinhdzai <hoangducbinh123123@gmail.com>
2024-01-04 22:55:42 +0700

    Solid Design Principles

---
+++ Design.Pattern/SOLID/Patterns/SRP.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+namespace SOLID.Patterns
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+
+        }
+    }
+
+    public class SRPDemo
+    {
+        public void Demo()
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var path = Directory.GetCurrentDirectory() + "/temp";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var filename = @$"{path}\journal.txt";
+            p.SaveToFile(j, filename);
+        }
+    }
+}
---
+++ Design.Pattern/SOLID/Program.cs
@@ -0,0 +1,19 @@
+﻿using SOLID.Patterns;
+
+Console.WriteLine("The SOLID Design Principles");
+
+//Single Responsibility Principle
+//var srpDemo = new SRPDemo();
+//srpDemo.Demo();
+
+//Open-Closed Principle
+//var ocpDemo = new OCPDemo();
+//ocpDemo.Demo();
+
+//Liskov Substitution Principle
+//var lspDemo = new LSPDemo();
+//lspDemo.Demo();
+
+
+var research = new Research();
+research.Demo();
