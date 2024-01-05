e9c8bdbacb4fd13577eb17ff03d808d36fac1e3b
embinhdzai <hoangducbinh123123@gmail.com>
2024-01-05 22:57:03 +0700

    Bulilder Pattern

---
+++ Design.Pattern/Builder/BuilderStepwise.cs
@@ -0,0 +1,63 @@
+﻿namespace Builder
+{
+    public enum CarType
+    {
+        Sedan,
+        Crossover
+    }
+
+    public class Car
+    {
+        public CarType Type;
+        public int WheelSize;
+    }
+
+    public interface ISpecifyCartype
+    {
+        ISpecifyWheelSize OfType(CarType type);
+    }
+
+    public interface ISpecifyWheelSize
+    {
+        IBuildCar WithWheels(int size);
+    }
+
+    public interface IBuildCar
+    {
+        public Car Build();
+    }
+
+    public class CarBuilder
+    {
+        private class Impl :
+            ISpecifyCartype,
+            ISpecifyWheelSize,
+            IBuildCar
+        {
+            private Car car = new Car();
+
+            public Car Build()
+            {
+                return car;
+            }
+
+            public ISpecifyWheelSize OfType(CarType type)
+            {
+                car.Type = type;
+                return this;
+            }
+
+
+            public IBuildCar WithWheels(int size)
+            {
+                car.WheelSize = size;
+                return this;
+            }
+        }
+
+        public static ISpecifyCartype Create()
+        {
+            return new Impl();
+        }
+    }
+}
---
+++ Design.Pattern/Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿//using Builder;
+
+//// Builder Interitance
+////var me = Person.New
+////  .Called("Dmitri")
+////  .WorksAsA("Quant")
+////  .Born(DateTime.UtcNow)
+////  .Build();
+////Console.WriteLine(me);
+
+////// Builder Stepwise
+////var car = CarBuilder.Create()
+////    .OfType(CarType.Crossover)
+////    .WithWheels(15)
+////    .Build();
+
+//// Builder Faceted
+//var pb = new PersonBuilder();
+//Person person = pb
+//  .Lives
+//    .At("123 London Road")
+//    .In("London")
+//    .WithPostcode("SW12BC")
+//  .Works
+//    .At("Fabrikam")
+//    .AsA("Engineer")
+//    .Earning(123000);
+
+//Console.WriteLine(person);
