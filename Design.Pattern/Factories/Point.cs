d7153aa623f9410b35591175c9a3e09790a47091
embinhdzai <hoangducbinh123123@gmail.com>
2024-01-09 02:17:30 +0700

    add Factory Pattern

---
+++ Design.Pattern/Factories/Point.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+    public class Point
+    {
+        private double x, y;
+
+        protected Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(double a,
+          double b, // names do not communicate intent
+          CoordinateSystem cs = CoordinateSystem.Cartesian)
+        {
+            switch (cs)
+            {
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    x = a;
+                    y = b;
+                    break;
+            }
+
+            // steps to add a new system
+            // 1. augment CoordinateSystem
+            // 2. change ctor
+        }
+
+        // factory property
+
+        public static Point Origin => new Point(0, 0);
+
+        // singleton field
+        public static Point Origin2 = new Point(0, 0);
+
+        // factory method
+
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        public enum CoordinateSystem
+        {
+            Cartesian,
+            Polar
+        }
+
+        // make it lazy
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+        }
+
+    }
+
+    class PointFactory
+    {
+        public static Point NewCartesianPoint(float x, float y)
+        {
+            return new Point(x, y); // needs to be public
+        }
+    }
+
+    class FactoryDemo
+    {
+        public void Demo()
+        {
+            //var p1 = new Point(2, 3, Point.CoordinateSystem.Cartesian);
+            //var origin = Point.Origin;
+
+            //var p2 = Point.Factory.NewCartesianPoint(1, 2);
+            var point = Point.Factory.NewCartesianPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point);
+        }
+    }
+}
---
+++ Design.Pattern/Factories/Program.cs
@@ -0,0 +1,25 @@
+﻿using Factories;
+
+// Factory
+//var factory = new TrackingThemeFactory();
+//var theme = factory.CreateTheme(true);
+//var theme2 = factory.CreateTheme(false);
+//Console.WriteLine(factory.Info);
+//// Dark theme
+//// Light theme
+
+
+//// replacement
+//var factory2 = new ReplaceableThemeFactory();
+//var magicTheme = factory2.CreateTheme(true);
+//Console.WriteLine(magicTheme.Value.BgrColor); // dark gray
+//factory2.ReplaceTheme(false);
+//Console.WriteLine(magicTheme.Value.BgrColor); // white
+
+// Abstract Factory Pattern
+var machine = new HotDrinkMachine();
+//var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 300);
+//drink.Consume();
+
+IHotDrink drink = machine.MakeDrink();
+drink.Consume();
